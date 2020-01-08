f00c86bc58482ab3aec279ad1c1d00bef069e2cb
Philip R Butler <philip-r-butler@gmail.com>
2020-01-08 20:30:23 +0000

    resolve linting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './assets/css/common.css';
 import Game from './components/Game';
 
-((global) => {
-    const game = new Game('canvas');
-    global.addEventListener('resize', game.resize, false);
+(global => {
+  const game = new Game('canvas');
+
+  global.addEventListener('resize', game.resize, false);
+
 })(window);
