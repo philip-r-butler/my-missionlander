de257ad43dce8c91e495dfbaeb34493e66ecb341
Philip R Butler <philip-r-butler@gmail.com>
2020-01-08 20:20:04 +0000

    add Draw class

---
+++ src/components/Draw/index.js
@@ -0,0 +1,19 @@
+const drawElements = context => ({
+  execute: obj =>
+    obj.forEach(element => command(context)[element.cmd](element)),
+});
+
+const command = context => ({
+  strokeStyle: ({ style }) => (context.strokeStyle = style),
+  beginPath: () => context.beginPath(),
+  moveTo: ({ x, y }) => context.moveTo(x, y),
+  lineTo: ({ x, y }) => context.lineTo(x, y),
+  lineWidth: ({ width }) => (context.lineWidth = width),
+  closePath: () => context.closePath(),
+  stroke: () => context.stroke(),
+  rect: ({ x, y, width, height }) => context.rect(x, y, width, height),
+  fillStyle: ({ style }) => (context.fillStyle = style),
+  fillRect: ({ x, y, width, height }) => context.fillRect(x, y, width, height),
+});
+
+export default drawElements;
