00b664fd4bceb11741d163a2a147a0ca6715b8ea
Philip R Butler <philip-r-butler@gmail.com>
2020-01-09 21:10:47 +0000

    expand commands with translate command

--- src/components/Draw/index.js
+++ src/components/Draw/index.js
@@ -7,6 +7,7 @@ const command = context => ({
   strokeStyle: ({ style }) => (context.strokeStyle = style),
   beginPath: () => context.beginPath(),
   moveTo: ({ x, y }) => context.moveTo(x, y),
+  translate: ({ x, y }) => context.translate(x, y),
   lineTo: ({ x, y }) => context.lineTo(x, y),
   lineWidth: ({ width }) => (context.lineWidth = width),
   closePath: () => context.closePath(),
