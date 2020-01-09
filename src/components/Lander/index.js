4fed581d3be381fede60a1bb6886554c31dc5e4f
Philip R Butler <philip-r-butler@gmail.com>
2020-01-09 21:14:12 +0000

    add move increment variables and update move method

--- src/components/Lander/index.js
+++ src/components/Lander/index.js
@@ -4,24 +4,34 @@ export default class Lander {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.increment = 1;
+    this.incrementX = 1;
+    this.incrementY = 0.02;
+    this.shape = Lander.shape;
   }
 
   move() {
-    this.increment *= 0.99;
-    this.x += this.increment;
-    this.y *= 1.01;
+    this.incrementY = this.incrementY * 1.009;
+    this.x += this.incrementX;
+    this.y += this.incrementY;
   }
 
   draw(context) {
-    const shape = [
-      { cmd: 'strokeStyle', style: 'white'},
-      { cmd: 'lineWidth', width: 1},
-      { cmd: 'beginPath' },
-      { cmd: 'rect', x: this.x, y: this.y, width: 10, height: 10 },
-      { cmd: 'stroke' },
-    ];
-
-    drawElements(context).execute(shape);
+    drawElements(context).execute(this.shape(this.x, this.y));
   }
 }
+
+Lander.shape = (x, y) => {
+  const tX = n => x + n;
+  const tY = n => y + n;
+
+  return [
+    { cmd: 'strokeStyle', style: 'white' },
+    { cmd: 'lineWidth', width: 1 },
+    { cmd: 'beginPath' },
+    { cmd: 'moveTo', x, y },
+    { cmd: 'lineTo', x: tX(5), y: tY(10) },
+    { cmd: 'lineTo', x: tX(-5), y: tY(10) },
+    { cmd: 'closePath' },
+    { cmd: 'stroke' },
+  ];
+};
