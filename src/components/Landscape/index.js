c1afc7da315a890042d953e76b117f811da08b03
Philip R Butler <philip-r-butler@gmail.com>
2020-01-14 14:39:22 +0000

    refactor: made Landscape.shape a method
    this is to make the Landscape.shape consistent with other shape objects

--- src/components/Landscape/index.js
+++ src/components/Landscape/index.js
@@ -1,16 +1,16 @@
 import GameObject from '../GameObject';
 
-export default class Landscape extends GameObject {
+class Landscape extends GameObject {
   constructor(x, y) {
-    super(x, y, Landscape.shape);
+    super(x, y, Landscape.shape());
   }
 }
 
-Landscape.shape = [
+Landscape.shape = () => [
   { cmd: 'strokeStyle', style: 'white' },
   { cmd: 'lineWidth', width: 1 },
   { cmd: 'beginPath' },
-  { cmd: 'moveTo', x: 0, y: 400 },
+  { cmd: 'moveTo', x: -1, y: 400 },
   { cmd: 'lineTo', x: 100, y: 500 },
   { cmd: 'lineTo', x: 150, y: 600 },
   { cmd: 'lineTo', x: 300, y: 500 },
@@ -20,5 +20,12 @@ Landscape.shape = [
   { cmd: 'lineTo', x: 840, y: 475 },
   { cmd: 'lineTo', x: 900, y: 500 },
   { cmd: 'lineTo', x: 1000, y: 600 },
+  { cmd: 'lineTo', x: 1000, y: 1000 },
+  { cmd: 'lineTo', x: -1, y: 1000 },
+  { cmd: 'closePath' },
+  { cmd: 'fillStyle', style: 'black' },
+  { cmd: 'fill' },
   { cmd: 'stroke' },
 ];
+
+export default Landscape;
