f3a08380e379933fce22c546197e691afdd57c78
Philip R Butler <philip-r-butler@gmail.com>
2020-01-15 16:16:48 +0000

    feat: add perlin noise
    implement more natural landscape shape using perlin noise module, made
    landscape extend on window resize

--- src/components/Landscape/index.js
+++ src/components/Landscape/index.js
@@ -1,31 +1,64 @@
+import * as Noise from 'perlin-noise-3d';
 import GameObject from '../GameObject';
 
 class Landscape extends GameObject {
-  constructor(x, y) {
-    super(x, y, Landscape.shape());
+  constructor(color, width, height) {
+    super();
+    this.width = width;
+    this.height = height;
+
+    this.makeShape();
+  }
+
+  makeShape() {
+    this.makePeaks();
+    super.shape = Landscape.shape(this.width, this.height, this.peaks);
+  }
+
+  makePeaks() {
+    const noise = new Noise();
+    noise.noiseSeed(Landscape.seed);
+
+    const points = Array.from({ length: this.width }, (_, increment) =>
+      noise.get(increment * Landscape.smoothness)
+    );
+    const minPoint = Math.min(...points);
+    const range = Math.max(...points) - minPoint;
+
+    this.peaks = points.map(point => this.height * (1 + Landscape.scale * (point - minPoint - range) / range));
+  }
+
+  setSize(width, height) {
+    super.setSize(width, height);
+    this.makeShape();
   }
 }
 
-Landscape.shape = () => [
-  { cmd: 'strokeStyle', style: 'white' },
-  { cmd: 'lineWidth', width: 1 },
-  { cmd: 'beginPath' },
-  { cmd: 'moveTo', x: -1, y: 400 },
-  { cmd: 'lineTo', x: 100, y: 500 },
-  { cmd: 'lineTo', x: 150, y: 600 },
-  { cmd: 'lineTo', x: 300, y: 500 },
-  { cmd: 'lineTo', x: 500, y: 700 },
-  { cmd: 'lineTo', x: 750, y: 850 },
-  { cmd: 'lineTo', x: 800, y: 450 },
-  { cmd: 'lineTo', x: 840, y: 475 },
-  { cmd: 'lineTo', x: 900, y: 500 },
-  { cmd: 'lineTo', x: 1000, y: 600 },
-  { cmd: 'lineTo', x: 1000, y: 1000 },
-  { cmd: 'lineTo', x: -1, y: 1000 },
-  { cmd: 'closePath' },
-  { cmd: 'fillStyle', style: 'black' },
-  { cmd: 'fill' },
-  { cmd: 'stroke' },
-];
+Landscape.seed = Math.random() * 100;
+Landscape.scale = 0.6;
+Landscape.smoothness = 0.003;
+
+Landscape.shape = (width = 0, height = 0, peaks = []) => {
+  const firstPeak = peaks[0];
+  const lastPeak = peaks[peaks.length - 1];
+  const start = [
+    { cmd: 'strokeStyle', style: 'white' },
+    { cmd: 'lineWidth', width: 1 },
+    { cmd: 'beginPath' },
+    { cmd: 'moveTo', x: -1, y: height + 1 },
+    { cmd: 'lineTo', x: -1, y: firstPeak },
+  ];
+  const points = peaks.map((y, x) => ({ cmd: 'lineTo', x, y }));
+  const end = [
+    { cmd: 'lineTo', x: width + 1, y: lastPeak },
+    { cmd: 'lineTo', x: width + 1, y: height + 1 },
+    { cmd: 'closePath' },
+    { cmd: 'fillStyle', style: 'black' },
+    { cmd: 'fill' },
+    { cmd: 'stroke' },
+  ];
+
+  return start.concat(points, end);
+};
 
 export default Landscape;
