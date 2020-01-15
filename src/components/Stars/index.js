853c82374cbcd6fe06347c236afeaab028a6cb10
Philip R Butler <philip-r-butler@gmail.com>
2020-01-15 15:38:53 +0000

    feat: add stars update on resize
    stars now recalculate and redraw on resize and change depending on
    window size, included a constant star density per pixel width

--- src/components/Stars/index.js
+++ src/components/Stars/index.js
@@ -1,32 +1,55 @@
 import GameObject from '../GameObject';
 
 class Stars extends GameObject {
-  constructor(number) {
-    super(0, 0, Stars.shape(number));
+  constructor(number, width, height) {
+    super(0, 0);
+    this.width = width;
+    this.height = height;
+    this.number = number;
+    super.shape = this.makeShape();
+  }
+
+  makeShape() {
+    return Stars.shape(
+      this.numberOfStars(this.number, this.width),
+      this.width,
+      this.height
+    );
+  }
+
+  numberOfStars(number, width) {
+    return Math.floor(number * width / 500);
+  }
+
+  setSize(width, height) {
+    super.setSize(width, height);
+    super.shape = this.makeShape();
   }
 }
 
-Stars.generateStars = number =>
-  [...Array(number)].map(() => [
-    {
-      cmd: 'rect',
-      x: Math.random() * 1000,
-      y: Math.random() * 500,
-      width: 2,
-      height: 2,
-    },
-    { cmd: 'fillStyle', style: 'white' },
-    { cmd: 'fill' },
-  ]).flat();
+Stars.makeStars = (number = 20, width = 1000, height = 500) =>
+  [...Array(number)]
+    .map(() => [
+      {
+        cmd: 'rect',
+        x: Math.random() * width,
+        y: Math.random() * height,
+        width: 2,
+        height: 2,
+      },
+      { cmd: 'fillStyle', style: 'white' },
+      { cmd: 'fill' },
+    ])
+    .flat();
 
-Stars.shape = (number = 10) => {
+Stars.shape = (number, width, height) => {
   const start = [
     { cmd: 'strokeStyle', style: 'white' },
     { cmd: 'lineWidth', width: 1 },
     { cmd: 'beginPath' },
   ];
   const end = [{ cmd: 'stroke' }];
-  return start.concat(Stars.generateStars(number), end);
+  return start.concat(Stars.makeStars(number, width, height), end);
 };
 
 export default Stars;
