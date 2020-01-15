972dbd227102a59901ff876655b19ba473549eb1
Philip R Butler <philip-r-butler@gmail.com>
2020-01-15 16:28:01 +0000

    refactor: use prototype
    moved from ES6 class to prototype declaration style of object, done to be consistent with other objects

--- src/components/GameObject/index.js
+++ src/components/GameObject/index.js
@@ -1,24 +1,22 @@
 import drawElements from '../Draw';
 
-class GameObject {
-  constructor(x, y, shape) {
-    this.x = x;
-    this.y = y;
-    this.shape = shape;
-  }
+const GameObject = function(x, y, shape) {
+  this.x = x;
+  this.y = y;
+  this.shape = shape;
+};
 
-  setSize(width, height) {
-    this.width = width;
-    this.height = height;
-  }
+GameObject.prototype.setSize = function(width, height) {
+  this.width = width;
+  this.height = height;
+};
 
-  draw(context) {
-    this.drawShape(context, this.shape);
-  }
+GameObject.prototype.draw = function(context) {
+  this.drawShape(context, this.shape);
+};
 
-  drawShape(context, shape) {
-    drawElements(context).execute(shape);
-  }
-}
+GameObject.prototype.drawShape = function(context, shape) {
+  drawElements(context).execute(shape);
+};
 
 export default GameObject;
