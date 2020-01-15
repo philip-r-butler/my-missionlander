55d69f8047143d6da0d72bcaa93737c80833733c
Philip R Butler <philip-r-butler@gmail.com>
2020-01-15 18:26:31 +0000

    refactor: use prototype
    moved from ES6 class to prototype declaration style of object, done to be consistent with other objects

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,32 +1,34 @@
 import GameObject from '../GameObject';
 
-class Background extends GameObject {
-  constructor(color, width, height) {
-    super();
-    this.x = 0;
-    this.y = 0;
-    this.color = color;
-    this.width = width;
-    this.height = height;
+const Background = function(color, width, height) {
+  GameObject.call(this, 0, 0, []);
+  this.x = 0;
+  this.y = 0;
+  this.color = color;
+  this.width = width;
+  this.height = height;
 
-    this.makeShape();
-  }
+  this.makeShape();
+};
 
-  makeShape() {
-    super.shape = Background.shape(
-      this.x,
-      this.y,
-      this.width,
-      this.height,
-      this.color
-    );
-  }
+Background.prototype = Object.create(GameObject.prototype);
 
-  draw(context) {
-    this.makeShape();
-    super.draw(context);
-  }
-}
+Background.prototype.constructor = Background;
+
+Background.prototype.makeShape = function() {
+  this.shape = Background.shape(
+    this.x,
+    this.y,
+    this.width,
+    this.height,
+    this.color
+  );
+};
+
+Background.prototype.draw = function(context) {
+  this.makeShape();
+  GameObject.prototype.drawShape(context, this.shape);
+};
 
 Background.shape = (x = 0, y = 0, width = 0, height = 0, color = '') => [
   { cmd: 'fillStyle', style: color },
