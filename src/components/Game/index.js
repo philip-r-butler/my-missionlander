cccb616596689176b257a21ba29a1b81b188b49d
Philip R Butler <philip-r-butler@gmail.com>
2020-01-15 12:38:57 +0000

    chore: convert from class to prototype declaration style
    decided to use prototype declaration as cleaner, cleaned up constructor method

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -3,100 +3,113 @@ import Lander from '../Lander';
 import Landscape from '../Landscape';
 import Stars from '../Stars';
 
-export default class Game {
-  constructor(global, canvasId) {
-    this.window = global;
-    this.canvas = this.window.document.getElementById(canvasId);
-    this.context = this.canvas.getContext('2d');
+function Game(global, canvasId) {
+  this.window = global;
+  this.canvas = this.window.document.getElementById(canvasId);
+  this.context = this.canvas.getContext('2d');
 
-    this.isUpPressed = false;
-    this.isLeftPressed = false;
-    this.isRightPressed = false;
-
-    this.setSize = this.setSize.bind(this);
-    this.animate = this.animate.bind(this);
-    this.onResize = this.onResize.bind(this);
-    this.onKeyDown = this.onKeyDown.bind(this);
-    this.onKeyUp = this.onKeyUp.bind(this);
-
-    this.window.addEventListener('resize', this.onResize, false);
-    this.window.addEventListener('keydown', this.onKeyDown, false);
-    this.window.addEventListener('keyup', this.onKeyUp, false);
-
-    this.create();
-    this.draw();
-  }
-
-  start() {
-    this.window.requestAnimationFrame(this.animate);
-  }
-
-  create() {
-    this.createGameObjects();
-    this.setSize();
-  }
-
-  createGameObjects() {
-    this.background = new Background('black', 0, 0);
-    this.landscape = new Landscape();
-    this.stars = new Stars(20);
-    this.lander = new Lander(110, 150);
-    //     this.platform = new GameObject(x, y, shape);
-  }
-
-  setSize() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    this.canvas.width = width;
-    this.canvas.height = height;
-    this.background.setSize(width, height);
-  }
-
-  onKeyDown(e) {
-    const keyCommands = {
-      ArrowUp: () => (this.isUpPressed = true),
-      ArrowLeft: () => {
-        this.isRightPressed = false;
-        this.isLeftPressed = true;
-      },
-      ArrowRight: () => {
-        this.isLeftPressed = false;
-        this.isRightPressed = true;
-      },
-    };
-    if (e.key in keyCommands) keyCommands[e.key]();
-  }
-
-  onKeyUp(e) {
-    const keyCommands = {
-      ArrowUp: () => (this.isUpPressed = false),
-      ArrowLeft: () => (this.isLeftPressed = false),
-      ArrowRight: () => (this.isRightPressed = false),
-    };
-    if (e.key in keyCommands) keyCommands[e.key]();
-  }
-
-  update() {
-    this.lander.move(this.isUpPressed, this.isRightPressed, this.isLeftPressed);
-  }
-
-  draw() {
-    this.background.draw(this.context);
-    this.stars.draw(this.context);
-    this.lander.draw(this.context);
-    this.landscape.draw(this.context);
-  }
-
-  onResize() {
-    this.setSize();
-    this.draw();
-  }
-
-  animate() {
-    this.update();
-    this.draw();
-
-    window.requestAnimationFrame(this.animate.bind(this));
-  }
+  this.setSize();
+  this.create();
+  this.draw();
 }
+
+Game.prototype.run = function() {
+  this.window.requestAnimationFrame(this.animate.bind(this));
+};
+
+Game.prototype.create = function() {
+  this.createEventListeners();
+  this.createKeyCommands();
+  this.createGameObjects();
+};
+
+Game.prototype.createEventListeners = function() {
+  this.window.addEventListener('resize', this.onResize.bind(this), false);
+  this.window.addEventListener('keydown', this.onKeyDown.bind(this), false);
+  this.window.addEventListener('keyup', this.onKeyUp.bind(this), false);
+};
+
+Game.prototype.createKeyCommands = function() {
+  this.isUpPressed = false;
+  this.isLeftPressed = false;
+  this.isRightPressed = false;
+
+  this.onKeyDownCommnands = {
+    ArrowUp: () => (this.isUpPressed = true),
+    ArrowLeft: () => {
+      this.isRightPressed = false;
+      this.isLeftPressed = true;
+    },
+    ArrowRight: () => {
+      this.isLeftPressed = false;
+      this.isRightPressed = true;
+    },
+  };
+
+  this.onKeyUpCommands = {
+    ArrowUp: () => (this.isUpPressed = false),
+    ArrowLeft: () => (this.isLeftPressed = false),
+    ArrowRight: () => (this.isRightPressed = false),
+  };
+};
+
+Game.prototype.createGameObjects = function() {
+  this.background = new Background(
+    'black',
+    this.canvas.width,
+    this.canvas.height
+  );
+  this.landscape = new Landscape(
+    'black',
+    this.canvas.width,
+    this.canvas.height
+  );
+  this.stars = new Stars(20, this.canvas.width, this.canvas.height);
+  this.lander = new Lander(110, 150);
+  //     this.platform = new GameObject(x, y, shape);
+};
+
+Game.prototype.setSize = function() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  this.canvas.width = width;
+  this.canvas.height = height;
+};
+
+Game.prototype.update = function() {
+  this.lander.move(this.isUpPressed, this.isRightPressed, this.isLeftPressed);
+};
+
+Game.prototype.draw = function() {
+  this.background.draw(this.context);
+  this.stars.draw(this.context);
+  this.lander.draw(this.context);
+  this.landscape.draw(this.context);
+};
+
+Game.prototype.animate = function() {
+  this.update();
+  this.draw();
+  window.requestAnimationFrame(this.animate.bind(this));
+};
+
+Game.prototype.onKeyDown = function(e) {
+  const keyCommands = this.onKeyDownCommnands;
+  if (e.key in keyCommands) keyCommands[e.key]();
+};
+
+Game.prototype.onKeyUp = function(e) {
+  const keyCommands = this.onKeyUpCommands;
+  if (e.key in keyCommands) keyCommands[e.key]();
+};
+
+Game.prototype.onResize = function() {
+  this.setSize();
+  this.background.setSize(this.canvas.width, this.canvas.height);
+  this.stars.setSize(this.canvas.width, this.canvas.height);
+  this.landscape.setSize(this.canvas.width, this.canvas.height);
+  this.draw();
+};
+
+export default Game;
