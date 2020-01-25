6623b300526db1053522eb0f3086519ef9733f17
Philip R Butler <philip-r-butler@gmail.com>
2020-01-25 23:59:23 +0000

    refactor: refactor landscape points from array to object
    made landscape points an object, changed to make collision detection
    easier (and possibly more performate)

--- src/components/Landscape/index.js
+++ src/components/Landscape/index.js
@@ -23,9 +23,14 @@ Landscape.prototype.makePoints = function() {
   const minPoint = Math.min(...data);
   const range = Math.max(...data) - minPoint;
 
-  this.points = data.map(
-    point => this.height * (1 + (Landscape.range * (point - minPoint - range)) / range)
-  );
+  this.points = data
+    .map((point, i) => {
+      return [i, this.height * (1 + (Landscape.range * (point - minPoint - range)) / range)];
+    })
+    .reduce((acc, item) => {
+      acc[item[0]] = item[1];
+      return acc;
+    }, {});
 };
 
 Landscape.prototype.makeShape = function() {
@@ -47,9 +52,10 @@ Landscape.seed = Math.random() * 100;
 Landscape.range = 0.6;
 Landscape.smoothness = 0.003;
 
-Landscape.shape = (width = 0, height = 0, peaks = []) => {
-  const firstPeak = peaks[0];
-  const lastPeak = peaks[peaks.length - 1];
+Landscape.shape = (width = 0, height = 0, points = []) => {
+  const firstPeak = points[0];
+  const keys = Object.keys(points);
+  const lastPeak = points[keys[[keys.length - 1]]];
   const start = [
     { cmd: 'strokeStyle', style: '#666666' },
     { cmd: 'lineWidth', width: 2 },
@@ -57,7 +63,7 @@ Landscape.shape = (width = 0, height = 0, peaks = []) => {
     { cmd: 'moveTo', x: -1, y: height + 1 },
     { cmd: 'lineTo', x: -1, y: firstPeak },
   ];
-  const points = peaks.map((y, x) => ({ cmd: 'lineTo', x, y }));
+  const lines = keys.map(key => ({ cmd: 'lineTo', x: key, y: points[key] }));
   const end = [
     { cmd: 'lineTo', x: width + 1, y: lastPeak },
     { cmd: 'lineTo', x: width + 1, y: height + 1 },
@@ -67,7 +73,7 @@ Landscape.shape = (width = 0, height = 0, peaks = []) => {
     { cmd: 'stroke' },
   ];
 
-  return start.concat(points, end);
+  return start.concat(lines, end);
 };
 
 export default Landscape;
