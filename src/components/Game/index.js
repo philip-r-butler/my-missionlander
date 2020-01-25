d3467a9dfd266197e6db7b3f4faaddf7bce869c5
Philip R Butler <philip-r-butler@gmail.com>
2020-01-25 23:54:16 +0000

    feat: add game information and collision
    add draw and update for game information and collision detection

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -3,6 +3,8 @@ import Lander from '../Lander';
 import Landscape from '../Landscape';
 import Platform from '../Platform';
 import Stars from '../Stars';
+import Collision from '../Collision';
+import GameInformation from '../GameInformation';
 
 function Game(global, canvasId) {
   this.window = global;
@@ -72,6 +74,8 @@ Game.prototype.createGameObjects = function() {
   );
   this.stars = new Stars(0.04, this.canvas.width, this.canvas.height);
   this.lander = new Lander(50, 50);
+  this.collision = new Collision();
+  this.gameInformation = new GameInformation(this.window);
 };
 
 Game.prototype.setSize = function() {
@@ -84,6 +88,7 @@ Game.prototype.setSize = function() {
 
 Game.prototype.update = function() {
   this.lander.move(this.isUpPressed, this.isRightPressed, this.isLeftPressed);
+  this.gameInformation.update(this.lander, this.landscape);
 };
 
 Game.prototype.draw = function() {
@@ -92,12 +97,36 @@ Game.prototype.draw = function() {
   this.platform.draw(this.context);
   this.landscape.draw(this.context);
   this.lander.draw(this.context);
+  this.gameInformation.draw();
 };
 
 Game.prototype.animate = function() {
+  const landerCanLand = this.lander.canLand();
+  const lander = this.lander.collisionPoints();
+  const isCollisionWithPlatform = this.collision.isCollision(
+    lander,
+    this.platform.points
+  );
+  const isCollisionWithLandscape = this.collision.isCollision(
+    lander,
+    this.landscape.points
+  );
+  const isCollision = isCollisionWithLandscape || isCollisionWithPlatform;
+  const isLanderOnPlatform = Object.keys(lander).every(
+    key => this.platform.points[Math.floor(key)]
+  );
+
+  const isCrashed = isCollision && !(isLanderOnPlatform && landerCanLand);
+  const isLanded = isCollision && isLanderOnPlatform && landerCanLand;
+
   this.update();
   this.draw();
-  window.requestAnimationFrame(this.animate.bind(this));
+
+  if (isLanded) console.log('LANDED');
+  if (isCrashed) console.log('BIG CRASH');
+  if (!isCrashed && !isLanded) {
+    window.requestAnimationFrame(this.animate.bind(this));
+  }
 };
 
 Game.prototype.onKeyDown = function(e) {
