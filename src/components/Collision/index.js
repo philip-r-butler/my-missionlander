7df0e0629401867b1e70b069b16e0e643d5e1286
Philip R Butler <philip-r-butler@gmail.com>
2020-02-03 14:38:00 +0000

    refactor: made collision object more generic
    did this to support particle collisions

--- src/components/Collision/index.js
+++ src/components/Collision/index.js
@@ -1,7 +1,34 @@
-const Collision = function() {};
+const Collision = function(obj1, obj2) {
+  this.obj1 = obj1;
+  this.obj2 = obj2;
+  this.exists = false;
+  this.x = undefined;
+  this.y = undefined;
 
-Collision.prototype.isCollision = function(projectile, boundary) {
-  return Object.keys(projectile).some(key => projectile[key] >= boundary[Math.floor(parseFloat(key))] - 1);
+  this.isCollision = this.isCollision.bind(this);
+};
+
+Collision.prototype.isCollision = function(key) {
+  return this.cp1[key] > this.cp2[Math.floor(parseFloat(key))];
+};
+
+Collision.prototype.isInside = function() {
+  return Object.keys(this.cp1).every(key => this.cp2[Math.floor(key)]);
+};
+
+Collision.prototype.update = function() {
+  this.cp1 = this.obj1.collisionPoints;
+  this.cp2 = this.obj2.collisionPoints;
+
+  this.exists = Object.keys(this.cp1).some(this.isCollision);
+
+  if (this.exists) {
+    this.x = Object.keys(this.cp1).filter(this.isCollision)[0];
+    this.y = this.cp2[this.x];
+  }
+
+  this.inside = this.isInside();
+  // console.log(this.inside);
 };
 
 export default Collision;
