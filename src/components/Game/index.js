fa7c391e87a3892abe066e9743b9b666a891f615
Philip R Butler <philip-r-butler@gmail.com>
2020-02-07 22:50:48 +0000

    refactor: refactor of mountain range, platforms, collision and explosion
    decided collision and explosion better located in lander object, made
    update method much simpler

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,29 +1,27 @@
 import Background from 'components/Background';
 import Lander from 'components/Lander';
-import Landscape from 'components/Landscape';
-import Platform from 'components/Platform';
 import Stars from 'components/Stars';
-import Collision from 'components/Collision';
-import Explosion from 'components/Explosion';
+import MountainRange from 'components/MountainRange';
+import LandingPlatform from 'components/LandingPlatform';
 import GameInformation from 'components/GameInformation';
 
 function Game(global, canvasId) {
   this.window = global;
   this.canvas = this.window.document.getElementById(canvasId);
   this.context = this.canvas.getContext('2d');
+
   this.winMessage = this.window.document.getElementById('win');
   this.looseMessage = this.window.document.getElementById('loose');
 
-  this.animate = this.animate.bind(this);
-
   this.createEventListeners();
-  this.createKeyCommands();
 
+  this.createKeyCommands();
   this.setSize();
+
+  this.animate = this.animate.bind(this);
 }
 
 Game.prototype.start = function() {
-
   this.winMessage.style.display = 'none';
   this.looseMessage.style.display = 'none';
   this.gameStopped = false;
@@ -54,9 +52,7 @@ Game.prototype.createKeyCommands = function() {
       this.isLeftPressed = false;
       this.isRightPressed = true;
     },
-    ' ': () => {
-      if (this.gameStopped) this.start();
-    },
+    ' ': () => this.gameStopped && this.start(),
   };
 
   this.onKeyUpCommands = {
@@ -72,23 +68,24 @@ Game.prototype.createGameObjects = function() {
     this.canvas.width,
     this.canvas.height
   );
-  this.landscape = new Landscape(
-    'black',
-    this.canvas.width,
-    this.canvas.height
-  );
-  this.platform = new Platform(
-    this.canvas.width,
-    this.canvas.height,
-    this.landscape
-  );
+  this.mountainRange = new MountainRange({
+    width: this.canvas.width,
+    height: this.canvas.height,
+    scale: 0.6,
+    smoothness: 0.003,
+    seed: 59.17262745556566,
+  });
+  this.landingPlatforms = new LandingPlatform(this.mountainRange.points);
+  this.makeCollisionLandscape();
   this.stars = new Stars(0.04, this.canvas.width, this.canvas.height);
-  this.lander = new Lander(50, 50);
-  this.crash = new Collision(this.lander, this.landscape);
-  this.landing = new Collision(this.lander, this.platform);
-  this.explosion = new Explosion(15, this.landscape);
+  this.lander = new Lander(
+    50,
+    50,
+    this.collisionLandscape,
+    this.mountainRange,
+    this.landingPlatforms
+  );
   this.gameInformation = new GameInformation(this.window);
-
 };
 
 Game.prototype.setSize = function() {
@@ -99,20 +96,33 @@ Game.prototype.setSize = function() {
   this.canvas.height = height;
 };
 
+Game.prototype.makeCollisionLandscape = function() {
+  this.collisionLandscape = {
+    collisionPoints: Object.assign(
+      {},
+      this.mountainRange.collisionPoints,
+      this.landingPlatforms.collisionPoints
+    ),
+  };
+};
+
 Game.prototype.update = function() {
   this.lander.update(this.isUpPressed, this.isRightPressed, this.isLeftPressed);
-  this.crash.update();
-  this.landing.update();
-  this.explosion.update();
-  if (!this.gameStopped) this.gameInformation.update(this.lander, this.landscape.collisionPoints);
+
+  // prettier-ignore
+  !this.gameStopped && this.gameInformation.update(this.lander, this.collisionLandscape);
+
+  if (this.lander.isCrashed || this.lander.isLanded) this.gameStopped = true;
+  if (this.lander.isLanded || this.lander.isCrashed && !this.lander.exploding) this.animationStopped = true;
+  this.lander.isCrashed && this.loose();
+  this.lander.isLanded && this.win();
 };
 
 Game.prototype.draw = function() {
   this.background.draw(this.context);
   this.stars.draw(this.context);
-  this.platform.draw(this.context);
-  this.explosion.draw(this.context);
-  this.landscape.draw(this.context);
+  this.landingPlatforms.draw(this.context);
+  this.mountainRange.draw(this.context);
   this.lander.draw(this.context);
   this.gameInformation.draw();
 };
@@ -126,33 +136,13 @@ Game.prototype.loose = function() {
 };
 
 Game.prototype.animate = function() {
-  if (!this.lander.isCrashed && !this.lander.isLanded) {
-    this.lander.isLanded = this.landing.exists && this.landing.inside && this.lander.canLand;
-    this.lander.isCrashed = this.crash.exists || (this.landing.exists && !(this.landing.inside && this.lander.canLand));
-  }
-
-  if (this.lander.isCrashed) {
-    this.gameStopped = true;
-    if (!this.explosion.active) this.explosion.explode(this.lander);
-    if (this.explosion.active && !this.explosion.isAlive()) this.animationStopped = true;
-    this.loose();
-  }
-
-  if (this.lander.isLanded) {
-    this.gameStopped = true;
-    this.animationStopped = true;
-    this.win();
-  }
-
   this.update();
   this.draw();
-
-  if (!this.animationStopped) this.window.requestAnimationFrame(this.animate);
+  !this.animationStopped && this.window.requestAnimationFrame(this.animate);
 };
 
 Game.prototype.onKeyDown = function(e) {
   const keyCommands = this.onKeyDownCommands;
-
   if (e.key in keyCommands) keyCommands[e.key]();
 };
 
@@ -165,8 +155,8 @@ Game.prototype.onResize = function() {
   this.setSize();
   this.background.setSize(this.canvas.width, this.canvas.height);
   this.stars.setSize(this.canvas.width, this.canvas.height);
-  this.landscape.setSize(this.canvas.width, this.canvas.height);
-  this.platform.setSize(this.canvas.width, this.canvas.height);
+  this.mountainRange.setSize(this.canvas.width, this.canvas.height);
+  this.landingPlatforms.setSize(this.canvas.width, this.canvas.height);
   this.draw();
 };
 
